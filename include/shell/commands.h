7d59f99ef67ea75e398b537939dad468fe2a8d0a
lavahead <123059883+vrified-stupd@users.noreply.github.com>
2024-08-22 17:44:17 +0800

    Update commands.h

--- include/shell/commands.h
+++ include/shell/commands.h
@@ -1,15 +1,15 @@
 // DON'T DELETE THIS FILE!
 #ifndef __ALL_HEADERS__
 #define __ALL_HEADERS__
-#include <term/viaSh/acom.h>
-#include <term/viaSh/cd.h>
-#include <term/viaSh/echo.h>
-#include <term/viaSh/help.h>
-#include <term/viaSh/hi.h>
-#include <term/viaSh/history.h>
-#include <term/viaSh/info.h>
-#include <term/viaSh/ld.h>
-#include <term/viaSh/math.h>
-#include <term/viaSh/read.h>
-#include <term/viaSh/shutdown.h>
+#include "term/viaSh/acom.h"
+#include "term/viaSh/cd.h"
+#include "term/viaSh/echo.h"
+#include "term/viaSh/help.h"
+#include "term/viaSh/hi.h"
+#include "term/viaSh/history.h"
+#include "term/viaSh/info.h"
+#include "term/viaSh/ld.h"
+#include "term/viaSh/math.h"
+#include "term/viaSh/read.h"
+#include "term/viaSh/shutdown.h"
 #endif
