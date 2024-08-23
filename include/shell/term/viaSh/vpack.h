ad2eadabac0bfe7f3bb6ec6de5507e383dbf7b44
bradinator <123034682+Ulladay-Hub@users.noreply.github.com>
2024-08-23 11:50:25 +1000

    Create placeholders for Vpack

---
+++ include/shell/term/viaSh/vpack.h
@@ -0,0 +1,17 @@
+// VPACKAGE MANAGER
+// Made by bradinator
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+void vpack_install(void);
+
+void vpack_update(void);
+
+void vpack_remove(void);
+
+void vpack_search(void);
+
+void vpack_list(void);
+
+#endif
