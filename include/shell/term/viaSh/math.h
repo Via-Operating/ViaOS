bcaf7e1524d688188db78a37dc3f92f1239442e9
lavahead <123059883+vrified-stupd@users.noreply.github.com>
2024-08-24 21:34:32 +0800

    Update math.h
    fixed my shit, and uhhhh, kap, your printfs dont support format specifiers

--- include/shell/term/viaSh/math.h
+++ include/shell/term/viaSh/math.h
@@ -1,43 +1,43 @@
 #include <via/stdio.h>
 
-int math(char* option, int num1, int num2) {
-    if (option == '-a') {
-        int output = num1 + num2;
+int math(char* option, float num1, float num2) {
+    if (option == "%c", 'a') {
+        float output = num1 + num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '-s') {
-        int output = num1 - num2;
+    else if (option == "%c", '-s') {
+        float output = num1 - num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '-m') {
-        int output = num1 * num2;
+    else if (option == "%c", '-m') {
+        float output = num1 * num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '-d') {
-        int output = num1 / num2;
+    else if (option == "%c", '-d') {
+        float output = num1 / num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '--add') {
-        int output = num1 + num2;
+    else if (option == "%s", '--add') {
+        float output = num1 + num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '--subtract') {
-        int output = num1 - num2;
+    else if (option == "%s", '--subtract') {
+        float output = num1 - num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '--multiply') {
-        int output = num1 * num2;
+    else if (option == "%s", '--multiply') {
+        float output = num1 * num2;
         printf(output);
         printf("> ");
     }
-    else if (option == '--divide') {
-        int output = num1 / num2;
+    else if (option == "%s", '--divide') {
+        float output = num1 / num2;
         printf(output);
         printf("> ");
     }
